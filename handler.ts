24eff076d5a3659e8b8b02fc2bf076bc1745b326
YAMAHATA Kenichiro <pokemogu@gmail.com>
2021-03-03 09:36:52 +0900

    チェックリストアイテムの期限通知機能を追加

--- handler.ts
+++ handler.ts
@@ -6,7 +6,7 @@ import { DynamoDB } from 'aws-sdk';
 const dynamo = new DynamoDB.DocumentClient();
 
 const getSettings = (name) => {
-  const params = { TableName: process.env.SETTINGS_TABLE,Key:{name:name} };
+  const params = { TableName: process.env.SETTINGS_TABLE, Key: { name: name } };
   return dynamo.get(params).promise();
 };
 
@@ -32,55 +32,57 @@ export const notify: APIGatewayProxyHandler = async (event, _context) => {
   const overdue_message_result = await getSettings('OverdueMessage');
   const target_boards_result = await getSettings('TargetBoards');
 
-  if(trello_api_key_result.Item)
+  if (trello_api_key_result.Item)
     trello_api_key = trello_api_key_result.Item.value;
   else
     throw new Error(`エラー: DynamoDBのテーブル${process.env.SETTINGS_TABLE}に'TrelloAPIKey'が設定されていません。`);
 
-  if(trello_api_token_result.Item)
+  if (trello_api_token_result.Item)
     trello_api_token = trello_api_token_result.Item.value;
   else
     throw new Error(`エラー: DynamoDBのテーブル${process.env.SETTINGS_TABLE}に'TrelloAPIToken'が設定されていません。`);
 
-  if(slack_webhook_result.Item)
+  if (slack_webhook_result.Item)
     slack_webhook_url = slack_webhook_result.Item.value;
   else
     throw new Error(`エラー: DynamoDBのテーブル${process.env.SETTINGS_TABLE}に'SlackWebhook'が設定されていません。`);
 
   const user_binds_result = await getUserBinds();
-  if(user_binds_result)
+  if (user_binds_result)
     trello_slack_user_bind = user_binds_result.Items;
   else
     throw new Error(`エラー: DynamoDBのテーブル${process.env.USERBINDS_TABLE}が設定されていません。`);
 
-  if(due_today_message_result.Item)
+  if (due_today_message_result.Item)
     slack_trello_task_due_today_message = due_today_message_result.Item.value;
   else
     slack_trello_task_due_today_message = 'チケットの対応期限が本日迄です。対応をお忘れなく。';
 
-  if(overdue_message_result.Item)
+  if (overdue_message_result.Item)
     slack_trello_task_overdue_message = overdue_message_result.Item.value;
   else
     slack_trello_task_overdue_message = 'チケットの対応期限が切れています。急ぎ対応をお願いします。';
 
-  if(target_boards_result.Item)
+  if (target_boards_result.Item)
     target_boards = target_boards_result.Item.value.split(',')
 
-  if(process.env.TRELLO_API_BASE_URL)
-    trello = new TrelloAPI(trello_api_key,trello_api_token,process.env.TRELLO_API_BASE_URL);
+  if (process.env.TRELLO_API_BASE_URL)
+    trello = new TrelloAPI(trello_api_key, trello_api_token, process.env.TRELLO_API_BASE_URL);
   else
-    trello = new TrelloAPI(trello_api_key,trello_api_token);
+    trello = new TrelloAPI(trello_api_key, trello_api_token);
 
   let slack = new SlackAPI(slack_webhook_url);
 
   const today: string = new Date().toISOString().split('T')[0];
+  //const today = new Date();
+  //today.setHours(0, 0, 0, 0);
 
   //const board_id = await trello.getBoardId('サンプルボード');
   const boards = await trello.getBoards();
-  for(let board of boards){
+  for (let board of boards) {
 
-    if(target_boards){
-      if(!target_boards.some((board_name) => board_name == board.name)){
+    if (target_boards) {
+      if (!target_boards.some((board_name) => board_name == board.name)) {
         continue;
       }
     }
@@ -89,47 +91,66 @@ export const notify: APIGatewayProxyHandler = async (event, _context) => {
     const memberships = await trello.getMembershipsOfBoard(board.id);
     const lists = await trello.getLists(board.id);
 
-    for(let list of lists){
+    for (let list of lists) {
       console.log(`\tEntering List name = ${list.name}, id = ${list.id}`);
       const cards = await trello.getCards(list.id);
-      const cards_due_today = cards.filter((v: any) => (v['dueComplete'] == false && v['due'] != null && v['due'].slice(0, 10) === today))
-      const cards_overdue = cards.filter((v: any) => (v['dueComplete'] == false && v['due'] != null && v['due'].slice(0, 10) < today))
+      const cards_due_today = cards.filter((v: any) => (v.dueComplete == false && v.due != null && v.due.slice(0, 10) === today));
+      const cards_overdue = cards.filter((v: any) => (v.dueComplete == false && v.due != null && v.due.slice(0, 10) < today));
 
       let cards_lists_due: any[] = [
-        [cards_due_today,slack_trello_task_due_today_message],
-        [cards_overdue,slack_trello_task_overdue_message],
+        [cards_due_today, slack_trello_task_due_today_message],
+        [cards_overdue, slack_trello_task_overdue_message],
       ];
 
       cards_lists_due.forEach((cards_list_due) => {
-        for(const card of cards_list_due[0]){
+        for (const card of cards_list_due[0]) {
           let message = '';
           let mention = '';
-          let trello_ids = card['idMembers'];
 
-          for(const trello_id of trello_ids){
-            console.log('trello_id = ' + trello_id);
-            let trello_username: string;
-            const trello_membership = memberships.find((membership:any) => membership.member.id === trello_id);
-
-            if(trello_membership){
-              trello_username = trello_membership.member.username;
-              console.log('trello_username = ' + trello_username);
-              const username_bind = trello_slack_user_bind.find((user:any) => user.trello_username === trello_username);
-
-              if(username_bind)
-                mention = `${mention}<@${username_bind.slack_memberid}>`;
-
-              if(mention != null)
-                message = `${mention}\n`;
-      
-              message = `${message}${cards_list_due[1]}\n`;
-              message = `${message}<${card['shortUrl']}|${card['name']}>`;
-              console.log(`message = ${message}`);
-              slack.sendMessage(message);
-            }
+          for (const trello_id of card.idMembers) {
+            const trello_membership = memberships.find((membership: any) => membership.member.id === trello_id);
+            const username_bind = trello_slack_user_bind.find((user: any) => user.trello_username === trello_membership.member.username);
+            if (username_bind)
+              mention = `${mention}<@${username_bind.slack_memberid}>`;
           }
+          if (mention != null)
+            message = `${mention}\n`;
+
+          message = `${message}${cards_list_due[1]}\n`;
+          message = `${message}<${card.shortUrl}|${card.name}>`;
+
+          slack.sendMessage(message);
         }
       });
+
+      const cards_due_checkitems = cards.filter((v: any) => (v.dueComplete == false && v.badges.checkItemsEarliestDue !== null && v.badges.checkItemsEarliestDue.slice(0, 10) <= today));
+      for (const card of cards_due_checkitems) {
+        const checklists = await trello.getCheckLists(card.id);
+
+        for (const checklist of checklists) {
+          const checkitems_due_today = checklist.checkItems.filter((v: any) => (v.state == 'incomplete' && v.due != null && v.due.slice(0, 10) === today));
+          const checkitems_overdue = checklist.checkItems.filter((v: any) => (v.state == 'incomplete' && v.due != null && v.due.slice(0, 10) < today));
+
+          let checkitems_lists_due: any[] = [
+            [checkitems_due_today, slack_trello_task_due_today_message],
+            [checkitems_overdue, slack_trello_task_overdue_message],
+          ];
+
+          checkitems_lists_due.forEach((checkitems_list_due) => {
+            for (const checkitem of checkitems_list_due[0]) {
+              const trello_membership = memberships.find((membership: any) => membership.member.id === checkitem.idMember);
+              const username_bind = trello_slack_user_bind.find((user: any) => user.trello_username === trello_membership.member.username);
+              let message = '';
+              if (username_bind)
+                message = `<@${username_bind.slack_memberid}>`;
+              console.log(checkitem);
+              message = `${message}${checkitems_list_due[1]}\n`;
+              message = `${message}<${card.shortUrl}|${card.name} ✓ ${checklist.name} - ${checkitem.name}>`;
+              slack.sendMessage(message);
+            }
+          });
+        }
+      }
     }
   }
 
--- trelloapi.ts
+++ trelloapi.ts
@@ -1,6 +1,6 @@
 import Axios, { AxiosInstance } from 'axios';
 
-export class TrelloAPI{
+export class TrelloAPI {
   protected axios: AxiosInstance;
   protected api_base_url: string;
   protected readonly api_get_boards_url_base: string = '/members/me/boards';
@@ -8,15 +8,19 @@ export class TrelloAPI{
   protected readonly api_get_cards_url_base: string = '/lists/{list_id}/cards';
   protected readonly api_get_member_url_base: string = '/members/{member_id}';
   protected readonly api_get_memberships_url_base: string = '/boards/{board_id}/memberships';
+  protected readonly api_get_checkitemstates_url_base: string = '/cards/{card_id}/checkItemStates';
+  protected readonly api_get_checklists_url_base: string = '/cards/{card_id}/checklists';
   protected api_get_boards_url: string;
   protected api_get_lists_url: string;
   protected api_get_cards_url: string;
   protected api_get_member_url: string;
   protected api_get_memberships_url: string;
+  protected api_get_checkitemstates_url: string;
+  protected api_get_checklists_url: string;
   private api_key: string;
   private api_token: string;
-  
-  public constructor(key: string,token: string,base_url: string = 'https://api.trello.com/1'){
+
+  public constructor(key: string, token: string, base_url: string = 'https://api.trello.com/1') {
     this.api_key = key;
     this.api_token = token;
     this.api_base_url = base_url;
@@ -25,60 +29,82 @@ export class TrelloAPI{
     this.api_get_cards_url = `${this.api_get_cards_url_base}?key=${this.api_key}&token=${this.api_token}`;
     this.api_get_member_url = `${this.api_get_member_url_base}?key=${this.api_key}&token=${this.api_token}`;
     this.api_get_memberships_url = `${this.api_get_memberships_url_base}?key=${this.api_key}&token=${this.api_token}`;
-  
+    this.api_get_checkitemstates_url = `${this.api_get_checkitemstates_url_base}?key=${this.api_key}&token=${this.api_token}`;
+    this.api_get_checklists_url = `${this.api_get_checklists_url_base}?key=${this.api_key}&token=${this.api_token}`;
+
     this.axios = Axios.create({
       baseURL: base_url,
-      headers: {'Content-Type':'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       responseType: 'json',
     });
   }
-  
-  public async getBoards(){
-    try{
+
+  public async getBoards() {
+    try {
       const response = await this.axios.get(this.api_get_boards_url);
       //const board = response.data.find(board => board.name === board_name);
       return response ? response.data : undefined;
-    }catch(error){
-      console.log(error);
-    }
-  }
-  
-  public async getLists(board_id: string){
-    try{
-      let url = this.api_get_lists_url.replace('{board_id}',board_id);
-      const response = await this.axios.get(url);
-      return response ? response.data : undefined;
-    }catch(error){
-      console.log(error);
-    }
-  }
-  
-  public async getCards(list_id: string){
-    try{
-      let url = this.api_get_cards_url.replace('{list_id}',list_id);
-      const response = await this.axios.get(url);
-      return response ? response.data : undefined;
-    }catch(error){
+    } catch (error) {
       console.log(error);
     }
   }
 
-  public async getMember(member_id: string){
-    try{
-      let url = this.api_get_member_url.replace('{member_id}',member_id);
+  public async getLists(board_id: string) {
+    try {
+      let url = this.api_get_lists_url.replace('{board_id}', board_id);
       const response = await this.axios.get(url);
       return response ? response.data : undefined;
-    }catch(error){
+    } catch (error) {
       console.log(error);
     }
   }
 
-  public async getMembershipsOfBoard(board_id: string){
-    try{
-      let url = this.api_get_memberships_url.replace('{board_id}',board_id) + '&member=true&member_fields=id,username';
+  public async getCards(list_id: string) {
+    try {
+      let url = this.api_get_cards_url.replace('{list_id}', list_id);
       const response = await this.axios.get(url);
       return response ? response.data : undefined;
-    }catch(error){
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async getMember(member_id: string) {
+    try {
+      let url = this.api_get_member_url.replace('{member_id}', member_id);
+      const response = await this.axios.get(url);
+      return response ? response.data : undefined;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async getMembershipsOfBoard(board_id: string) {
+    try {
+      let url = this.api_get_memberships_url.replace('{board_id}', board_id) + '&member=true&member_fields=id,username';
+      const response = await this.axios.get(url);
+      return response ? response.data : undefined;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async getCheckItemStates(card_id: string) {
+    try {
+      let url = this.api_get_checkitemstates_url.replace('{card_id}', card_id);
+      const response = await this.axios.get(url);
+      return response ? response.data : undefined;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async getCheckLists(card_id: string) {
+    try {
+      let url = this.api_get_checklists_url.replace('{card_id}', card_id);
+      const response = await this.axios.get(url);
+      return response ? response.data : undefined;
+    } catch (error) {
       console.log(error);
     }
   }
